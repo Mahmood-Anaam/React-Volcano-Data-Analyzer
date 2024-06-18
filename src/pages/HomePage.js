a9dba0e0f061001c93416342cf05c6009a00c236
Eng.Mahmood Anaam <eng.mahmood.anaam@gmail.com>
2024-06-18 17:16:45 +0300

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -9,7 +9,7 @@ const HomePage = () => {
       <Container className="text-center hero-content">
         <h1>Welcome to Volcano App</h1>
         <p>Explore volcanoes of the world and gain insights!</p>
-        <Button color="primary" href="VolcanoListPage.js" className="m-2">
+        <Button color="primary" href="/volcano-list" className="m-2">
           Explore Volcano List
         </Button>
       </Container>
